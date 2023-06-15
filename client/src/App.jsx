52276aecf7099635b8b61b4cbadcf6b36c7df158
ronnie-miller <jovi@Jovi-Desktop>
2023-06-15 13:12:53 -0700

    created auth file and functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,4 @@
-
-import HpCarousel from "./components/landing-page/carousel";
 import React from "react";
-
 import Navbar from "./components/nav-footer/Navbar";
 import Footer from "./components/nav-footer/Footer";
 
@@ -10,7 +7,7 @@ function App() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-grow">{/* Your app content */}</div>
-      <HpCarousel />
+
       <Footer />
     </div>
   );
