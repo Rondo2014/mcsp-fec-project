0502e889ff9c0166519b5e8997fd5560cc32f659
ronnie-miller <jovi@Jovi-Desktop>
2023-06-15 13:37:58 -0700

    modified auth utility.

--- server/auth.js
+++ server/auth.js
@@ -1,16 +1,22 @@
-import { Jwt } from "jsonwebtoken";
+import { jwt } from "jsonwebtoken";
 import { bcrypt } from "bcrypt";
 
 const saltRounds = 10;
 
+// When a login route is created, use this middleware on that route
 export const hasPasswordMiddleware = (req, res, next) => {
+  // get password from request body
   const { password } = req.body;
+  // optional password length check
   if (password && password.length > 5) {
+    // hash password using bcrypt
     bcrypt.hash(password, saltRounds, (err, hash) => {
       if (err) {
         return res.status(500).json({ err: "Error hashing password" });
       }
+      // set password to hashed password
       req.body.password = hashedPassword;
+      //continue with the post request
       next();
     });
   } else {
@@ -20,6 +26,7 @@ export const hasPasswordMiddleware = (req, res, next) => {
 
 export const validateToken = (token, secretKey) => {
   try {
+    // verify token using jwt
     const decoded = jwt.verify(token, secretKey);
     return { valid: true, decoded };
   } catch (err) {
@@ -28,5 +35,6 @@ export const validateToken = (token, secretKey) => {
 };
 
 export const validatePassword = (username, password) => {
+  // compares password to hashed password using bcrypt
   return bcrypt.compare(password, user.password);
 };
