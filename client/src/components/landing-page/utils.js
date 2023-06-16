9cd20247184134ee448e8c94d737bac716b2ac87
ronnie-miller <jovi@Jovi-Desktop>
2023-06-16 12:15:36 -0700

    finished initial carousel layout

--- client/src/components/landing-page/utils.js
+++ client/src/components/landing-page/utils.js
@@ -38,3 +38,50 @@ export const SIDE_BAR_GENRES = [
   "Sports",
   "Strategy",
 ];
+
+export const SPECIAL_CAROUSEL_DATA = [
+  {
+    id: 1,
+    items: [
+      {
+        content:
+          "https://cdn.cloudflare.steamstatic.com/steam/spotlights/dde6d5bfa7176ce51b23773f/spotlight_image_english.jpg?t=1686684470",
+        title: "title1",
+        description: "description1",
+        link: "link1",
+        discount: "-30%",
+        offer: "Offer ends Jun 19 @ 10:00am.",
+      },
+      {
+        content:
+          "https://cdn.cloudflare.steamstatic.com/steam/spotlights/1cd1d5285b9d7179fe95b962/spotlight_image_english.jpg?t=1686347748",
+        title: "title2",
+        description: "description2",
+        link: "link2",
+        discount: "-30%",
+        offer: "Offer ends Jun 19 @ 10:00am.",
+      },
+      {
+        content:
+          "https://cdn.cloudflare.steamstatic.com/steam/spotlights/2b7f5b0b7e2b3e2b7e2b3e2b/spotlight_image_english.jpg?t=1686684470",
+        title: "title3",
+        description: "description3",
+        link: "link3",
+        discount: "-30%",
+        offer: "Offer ends Jun 19 @ 10:00am.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    items: [
+      {
+        content:
+          "https://cdn.cloudflare.steamstatic.com/steam/spotlights/2b7f5b0b7e2b3e2b7e2b3e2b/spotlight_image_english.jpg?t=1686684470",
+        title: "title2",
+        description: "description2",
+        link: "link2",
+      },
+    ],
+  },
+];
