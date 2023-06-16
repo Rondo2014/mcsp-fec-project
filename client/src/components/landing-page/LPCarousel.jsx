61865072a78021f08a58a98afd4f7d81aa7d68cc
zemitswasfuh <darrin.m.mosher@gmail.com>
2023-06-16 09:43:56 -0700

    carousel needs hover function fix

--- client/src/components/landing-page/LPCarousel.jsx
+++ client/src/components/landing-page/LPCarousel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -13,7 +13,26 @@ const carouselData = [
   },
 ];
 
+const miniPics = [{
+  image: "https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art-square_b893fc9e.jpeg?region=0%2C0%2C1080%2C1080"
+},
+{
+  image: "https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art-square_b893fc9e.jpeg?region=0%2C0%2C1080%2C1080"
+},
+{
+  image: "https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art-square_b893fc9e.jpeg?region=0%2C0%2C1080%2C1080"
+},
+{
+  image: "https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art-square_b893fc9e.jpeg?region=0%2C0%2C1080%2C1080"
+}]
+
 const LPCarousel = () => {
+  const [carouselImage, setCarouselImage] = useState(carouselData[0].image);
+
+  const handleImageHover = (image) => {
+    setCarouselImage(image);
+  };
+
   return (
     <>
     <div className="flex items-center justify-center">
@@ -31,13 +50,18 @@ const LPCarousel = () => {
         ))}
       </Carousel>
       <div className="grid grid-cols-2 gap-2 pb-5 pl-1">
-        <img className="w-40 h-20 object-cover" src="https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art-square_b893fc9e.jpeg?region=0%2C0%2C1080%2C1080" alt="minipic"/>
-        <img className="w-40 h-20 object-cover" src="https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art-square_b893fc9e.jpeg?region=0%2C0%2C1080%2C1080" alt="minipic"/>
-        <img className="w-40 h-20 object-cover" src="https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art-square_b893fc9e.jpeg?region=0%2C0%2C1080%2C1080" alt="minipic"/>
-        <img className="w-40 h-20 object-cover" src="https://lumiere-a.akamaihd.net/v1/images/star-wars-outlaws-key-art-square_b893fc9e.jpeg?region=0%2C0%2C1080%2C1080" alt="minipic"/>
+        {miniPics.map((item, index) => (
+          <img 
+          className="w-40 h-20 object-cover"
+          key={index}
+          src={item.image}
+          alt={`Slide ${index + 1}`}
+          onMouseEnter={() => handleImageHover(item.image)}
+          onMouseLeave={() => handleImageHover(null)}
+          />
+        ))}
       </div>
     </div>
-
     </>
   );
 };
