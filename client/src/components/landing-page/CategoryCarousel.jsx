ab0cfa6375a2980f97ec3fe9d53c7316a2020f9c
zemitswasfuh <darrin.m.mosher@gmail.com>
2023-06-20 12:04:07 -0700

    starting from scratch

--- client/src/components/landing-page/CategoryCarousel.jsx
+++ client/src/components/landing-page/CategoryCarousel.jsx
@@ -1,105 +1,78 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { CATEGORIES } from "./utils";
-import api from "../../api/axios.js"
 
 const CategoryCarousel = () => {
-  const [activeCarousel, setActiveCarousel] = useState(1);
-  const [category, setCategory] = useState([]);
+  const [currentCarousel, setCurrentCarousel] = useState(1);
 
-console.log('category', category)
-
-  const chunk = (arr, size) => {
-    const newArr = [];
-    let id = 1;
-    for (let i = 0; i < arr.length; i += size) {
-      const chunk = {
-        id: id,
-        items: arr.slice(i, i + size),
-      };
-      newArr.push(chunk);
-      id++;
-    }
-    return newArr;
+  const handleLeftArrowClick = () => {
+    setCurrentCarousel((prev) => (prev - 1 + CATEGORIES.length) % CATEGORIES.length + 1);
   };
 
-  useEffect(() => {
-    try {
-      api.get("/api/games/categories").then((res) => {
-        const category = chunk(res.data, 4);
-        setCategory(category);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
-  const handleNext = () => {
-    setCarouselSlide((prevIndex) => (prevIndex + 1) % carouselData.length);
+  const handleRightArrowClick = () => {
+    setCurrentCarousel((prev) => (prev % CATEGORIES.length) + 1);
   };
 
-  const handlePrev = () => {
-    setCarouselSlide(
-      (prevIndex) => (prevIndex - 1 + carouselData.length) % carouselData.length
-    );
-  };
+  // const chunk = (arr, size) => {
+  //   const newArr = [];
+  //   let id = 1;
+  //   for (let i = 0; i < arr.length; i += size) {
+  //     const chunk = {
+  //       id: id,
+  //       items: arr.slice(i, i + size),
+  //     };
+  //     newArr.push(chunk);
+  //     id++;
+  //   }
+  //   return newArr;
+  // };
+
+  // useEffect(() => {
+  //   try {
+  //     api.get("/api/games/featured").then((res) => {
+  //       const featuredGames = chunk(res.data, 4);
+  //       setFeaturedGames(featuredGames);
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, []);
+
 
   return (
-    <div className="carousel-container overflow-x-auto p-4 text-cyan-50 my-8">
-      <h2 className="uppercase my-8 font-semibold leading-4">
-        Browse By Category
-      </h2>
-      {/* <div
-        id="arrow-left"
-        className="left-[-46px] absolute top-[38%] w-auto h-auto py-9 px-[11px] cursor-pointer"
-        style={{
-          background:
-            "linear-gradient( to right, rgba( 0, 0, 0, 0.3) 5%,rgba( 0, 0, 0, 0) 95%)",
-          backgroundSize: "cover",
-        }}
-        onClick={handleMinusCarousel}
+    <div className="flex items-center  text-white pt-7">
+      <div
+        className="left-arrow p-2 cursor-pointer"
+        onClick={handleLeftArrowClick}
       >
-        <div
-          style={{
-            backgroundImage:
-              "url(https://store.cloudflare.steamstatic.com/public/images/v6/arrows.png)",
-          }}
-          className="w-[23px] h-9 transform rotate-180"
-        ></div>
+        Left Arrow
+      </div>
+      <div className="carousel">
+        <div className="carouselInner">
+          <div className="carousel-content flex space-x-4">
+            {CATEGORIES.map((category, index) => (
+              <div
+                key={category.id}
+                className={`carousel-item flex flex-col items-center ${
+                  index + 1 === currentCarousel ? "active" : ""
+                }`}
+              >
+                <img
+                  src={category.image}
+                  alt={category.name}
+                  className="w-48 h-32 object-cover rounded-lg hover:shadow-md hover:shadow-[#1A9FFF] mr-2 hover:scale-105 transition duration-300 ease-in-out cursor-pointer"
+                />
+                <p className="mt-2 text-lg font-semibold">{category.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
       <div
-        id="arrow-right"
-        className="right-[-46px] absolute top-[38%] w-auto h-auto py-9 px-[11px] cursor-pointer"
-        style={{
-          background:
-            "linear-gradient( to left, rgba( 0, 0, 0, 0.3) 5%,rgba( 0, 0, 0, 0) 95%)",
-          backgroundSize: "cover",
-        }}
-        onClick={handleAddCarousel}
+        className="right-arrow p-2 cursor-pointer"
+        onClick={handleRightArrowClick}
       >
-        <div
-          style={{
-            backgroundImage:
-              "url(https://store.cloudflare.steamstatic.com/public/images/v6/arrows.png)",
-          }}
-          className="w-[23px] h-9"
-        ></div>
-      </div> */}
-      <div className="carousel-content flex space-x-4">
-        {CATEGORIES.map((category) => (
-          <div
-            key={category.id}
-            className="carousel-item flex flex-col items-center"
-          >
-            <img
-              src={category.image}
-              alt={category.name}
-              className="w-48 h-32 object-cover rounded-lg hover:shadow-md hover:shadow-[#1A9FFF] mr-2 hover:scale-105 transition duration-300 ease-in-out cursor-pointer"
-            />
-            <p className="mt-2 text-lg font-semibold">{category.name}</p>
-          </div>
-          
-        ))}
+        Right Arrow
       </div>
     </div>
   );
