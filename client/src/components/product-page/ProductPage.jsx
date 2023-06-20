4c557438502da1bef3054399ec363bc3e383730b
ronnie-miller <jovi@Jovi-Desktop>
2023-06-20 09:37:08 -0700

    Added image to right col

--- client/src/components/product-page/ProductPage.jsx
+++ client/src/components/product-page/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import StoreNavBar from "../landing-page/StoreNavBar";
 import TitleBar from "./TitleBar";
+import ProductCarousel from "./ProductCarousel";
 import api from "../../api/axios";
 import { useParams } from "react-router-dom";
 
@@ -26,6 +27,7 @@ const ProductPage = () => {
     <>
       <StoreNavBar />
       <TitleBar game={game} />
+      <ProductCarousel game={game} />
     </>
   );
 };
