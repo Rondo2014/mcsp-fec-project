c63ace17aa2799a327d66f5383b1b5fccd5b0d1f
ronnie-miller <jovi@Jovi-Desktop>
2023-06-20 07:35:58 -0700

    Added react router dom and product link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from "./components/nav-footer/Navbar";
 import Footer from "./components/nav-footer/Footer";
+import ProductPage from "./components/product-page/ProductPage";
 import LandingPage from "./components/landing-page/LandingPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className=" min-h-screen">
       <Navbar />
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/product" element={<ProductPage />} />
+      </Routes>
       <Footer />
     </div>
   );
