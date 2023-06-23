b3099d983f8506ecb31af1e0d6da201e9c4eb377
Triston117 <roviiknaris@yahoo.com>
2023-06-23 15:13:55 -0700

    Changes lol

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex-grow">{/* Your app content */}</div>
+      <div className="flex-grow"></div>
       <Footer />
     </div>
   );
